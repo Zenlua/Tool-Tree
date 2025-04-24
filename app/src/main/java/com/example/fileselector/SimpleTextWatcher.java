611d704752736dfe1cfa2a6d49de37230086926d
Kẹo Kéo <kakathic@gmail.com>
2025-04-24 19:38:12 +0700

    Create SimpleTextWatcher.java

---
+++ app/src/main/java/com/example/fileselector/SimpleTextWatcher.java
@@ -0,0 +1,19 @@
+package com.example.fileselector;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+
+public abstract class SimpleTextWatcher implements TextWatcher {
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {}
+
+    @Override
+    public void afterTextChanged(Editable s) {
+        onTextChanged(s.toString());
+    }
+
+    public abstract void onTextChanged(String text);
+}
