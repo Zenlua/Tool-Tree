482954d956acc08a912fde9b5dbfb479dde34269
Kakathic <kakathic@gmail.com>
2025-12-20 13:55:24 +0700

    Update menu.sh

--- add-on/features/menu.sh
+++ add-on/features/menu.sh
@@ -1,2 +1,9 @@
 source language 2>/dev/null
-echo '<option type="refresh">'$refresh_text'</option>'
+echo '
+<option type="default" id="v1" auto-off="true" reload="true" interruptible="false" >'$google_translate_text'</option>
+<handler>
+if [ "$menu_id" == "v1" ];then
+[ "$(glog auto_trans_text)" == 1 ] && slog auto_trans_text 0 || slog auto_trans_text 1
+fi
+</handler>
+'
