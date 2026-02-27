18b96a0535f1fe47b8d0e2f1e858f8b377c2613b
Zenlua <kakathic@gmail.com>
2026-02-28 01:25:42 +0700

    Upload file

--- pio/src/main/assets/module/ZADD/payload_online/index.sh
+++ pio/src/main/assets/module/ZADD/payload_online/index.sh
@@ -1,60 +1,77 @@
 #!/data/data/com.tool.tree/files/home/bin/bash
 # Kakathic
 
-#current directory
-MPAT="${0%/*}"
-if [ -n "$(glog url_text_payload)" ];then
-checkdjhrh=1
-    if [ "$(glog url_text_payload | checksum)" != "$(cat $MPAT/pmd)" ];then
-    payload_dumper --out $TMP --list "$(glog url_text_payload)" | sed "1,/-----/d" | awk '{print $1"|"$1" ("$2$3")"}' > $MPAT/list
-    glog url_text_payload | checksum > $MPAT/pmd
-    fi
-else
-checkdjhrh=0
-fi
-
-# Dịch tự động
-source $MPAT/addon.prop
-sum_md5_small="$(checksum $MPAT/addon.prop)"
-if [[ "$sum_md5_small" != "$(cat $MPAT/md5)" ]] || [[ ! -f $MPAT/auto.sh ]] || [[ "$(grep -cm1 '=\"\"' $MPAT/auto.sh)" == 1 ]];then
-[ -f $MPAT/auto.sh ] && rm -fr $MPAT/auto.sh
-for vc in $(grep "_text_.*.=" $MPAT/addon.prop | cut -d= -f1); do
-echo "${vc}=\"$(echo "${!vc}" | trans $LANGUAGE-$COUNTRY | awk '{ $0 = toupper(substr($0,1,1)) substr($0,2); print }')\" #${!vc}" >>$MPAT/auto.sh &
-done
-    wait
-    if [[ "$(grep -cm1 '=\"\"' $MPAT/auto.sh)" == 1 ]];then
-    sed -i '/=\"\"/d' $MPAT/auto.sh
-    else
-    chmod 755 $MPAT/auto.sh
-    echo -n "$sum_md5_small" > $MPAT/md5
-    fi
-fi
-[[ -f $MPAT/auto.sh ]] && source $MPAT/auto.sh
-
+home(){
 # index
-echo '<?xml version="1.0" encoding="UTF-8" ?>
-<page>
-
+echo '
 <group>
 <action shell="hidden" reload="true">
 <title>'$payload_text_1'</title>
-<summary>'$payload_text_2' '"$(glog url_text_payload)"'</summary>
-<param name="url_text_payload" value-sh="glog url_text_payload" type="text" required="required" />
+<summary>'$payload_text_2' '"http://...$(glog url_text_payload | tail -c 25)"'</summary>
+<param name="url_text_payload" placeholder="https://web.com/rom-payload-ota.zip" value-sh="glog url_text_payload" type="text" required="required" />
 <set>slog url_text_payload "$url_text_payload"</set>
 </action>
 </group>
 
 <group>
-<action reload="true" visible="echo '$checkdjhrh'">
+<action visible="echo '$checkdjhrh'" reload="true">
 <title>'$payload_text_3'</title>
 <summary>'$payload_text_4' '$PTSD'</summary>
-<param name="partition" desc="'$payload_text_5'" multiple="multiple" separator="," options-sh="cat '$MPAT'/list"/>
+<param name="partition" desc="'$payload_text_5'" multiple="multiple" options-sh="cat '$MPAT'/list_payload" required="required"/>
 <set>
 echo "Downloading..." | trans -b $LANGUAGE-$COUNTRY
 echo
-payload_dumper --out "$PTSD" -i "$partition" "$(glog url_text_payload)" | trans -b $LANGUAGE-$COUNTRY
+for vv in $partition; do
+'$MPAT'/payload.sh $vv
+done
 </set>
 </action>
-</group>
+</group>' | sed -z -e 's|\&|\&amp;|g' -e 's|§|\&#xA;|g'
+}
 
-</page>' | sed -z -e 's|\&|\&amp;|g' -e 's|§|\&#xA;|g'
+# Thư mục hiện tại
+MPAT="${0%/*}"
+if [ -n "$(glog url_text_payload)" ];then
+checkdjhrh=1
+    if [ "$(glog url_text_payload | checksum)" != "$(glog url_text_payload_md5)" ];then
+    listpayload "$(glog url_text_payload)" | awk '{print $1"|"$1" "$2}' > $MPAT/list_payload
+    slog url_text_payload_md5 "$(glog url_text_payload | checksum)"
+    fi
+else
+checkdjhrh=0
+fi
+
+# Ngôn ngữ mặc định
+eval "$(sed '1,/root=/d' $MPAT/addon.prop)"
+
+# Dịch tự động
+if [ "$(glog "auto_trans_text_${MPAT##*/}")" == 1 ];then
+sum_md5="$(sha256sum $MPAT/addon.prop | awk '{print $1}')"
+if [[ "$sum_md5" != "$(glog "sum_md5_${MPAT##*/}")" ]] || [[ ! -f $MPAT/auto.sh ]] || [[ "$(grep -cm1 '=\"\"' $MPAT/auto.sh)" == 1 ]];then
+[ -f $MPAT/auto.sh ] && rm -fr $MPAT/auto.sh
+for vc in $(grep -e '="' $MPAT/addon.prop | cut -d= -f1); do
+echo -e "${vc}=\"$(echo "${!vc}" | trans $LANGUAGE-$COUNTRY | awk '{ $0 = toupper(substr($0,1,1)) substr($0,2); print }')\" #${!vc}" >>$MPAT/auto.sh &
+done
+    wait
+    if [[ "$(grep -cm1 '=\"\"' $MPAT/auto.sh)" == 1 ]];then
+    sed -i '/=\"\"/d' $MPAT/auto.sh
+    else
+    slog "sum_md5_${MPAT##*/}" "$sum_md5"
+    fi
+fi
+[[ -f $MPAT/auto.sh ]] && source $MPAT/auto.sh
+trans_text="$google_text"
+fi
+
+# index
+echo '<?xml version="1.0" encoding="UTF-8" ?>
+<group>'
+case "$1" in
+    home)
+        "$1"
+        ;;
+    *)
+        cat "$ETC/error.xml"
+        ;;
+esac
+echo '</group>'
---
+++ pio/src/main/assets/module/ZADD/payload_online/payload.sh
@@ -0,0 +1,17 @@
+# Kakathic
+set -o pipefail
+num1="$TMP/url_payload_extract.log";
+[ -f "$num1" ] && rm -f "$num1"
+payload_extract -o "$PTSD" -X "$1" -i "$(glog url_text_payload)" 2>&1 | tee "$TMP/url_payload_extract.log" || killtree "Download network error, Please delete the file and download it again." &
+PIDK=$!
+while kill -0 $PIDK 2>/dev/null; do
+if [ -f "$num1" ];then
+    num3=$(tr '\r' '\n' < "$num1" | grep -o '[0-9]\+%' | tail -n1 | tr -d '%')
+    if [ "$num3" ];then
+    [ "${num3:-0}" == "${num4:-1}" ] || progress "$num3/100"
+    num4="$num3"
+    fi
+fi
+sleep 1
+done
+progress -1/0
