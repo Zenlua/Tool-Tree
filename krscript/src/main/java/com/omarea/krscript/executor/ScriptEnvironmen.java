20856797ab48c7fc3a4a6bf266ef11a98a68d0e9
Zenlua <kakathic@gmail.com>
2026-03-11 17:33:01 +0700

    Upload file

--- krscript/src/main/java/com/omarea/krscript/executor/ScriptEnvironmen.java
+++ krscript/src/main/java/com/omarea/krscript/executor/ScriptEnvironmen.java
@@ -29,8 +29,7 @@ import android.content.res.Resources;
 import java.util.TimeZone;;
 import java.lang.Runtime;
 import java.nio.file.Paths;
-import android.widget.Toast;
-import android.os.SELinux;
+import android.widget.Toast;;
 
 public class ScriptEnvironmen {
     private static final String ASSETS_FILE = "file:///android_asset/";
@@ -254,7 +253,6 @@ public class ScriptEnvironmen {
         params.put("PACKAGE_NAME", context.getPackageName());
         params.put("PACKAGE_VERSION_NAME", packageInfo.versionName);
         params.put("PATH_APK", context.getApplicationInfo().sourceDir);
-        params.put("APP_CONTEXT", SELinux.getFileContext(context.getFilesDir().getAbsolutePath()));
         params.put("APP_UID", String.valueOf(android.os.Process.myUid()));
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
                 params.put("PACKAGE_VERSION_CODE", String.valueOf(packageInfo.getLongVersionCode()));
