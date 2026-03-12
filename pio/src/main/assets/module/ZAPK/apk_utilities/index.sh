2054a57e0c0715f15d2569f1affef1c0f16c68fd
Zenlua <kakathic@gmail.com>
2026-03-12 18:05:41 +0700

    Upload file

--- pio/src/main/assets/module/ZAPK/apk_utilities/index.sh
+++ pio/src/main/assets/module/ZAPK/apk_utilities/index.sh
@@ -1,18 +1,34 @@
 #!/data/data/com.tool.tree/files/home/bin/bash
 # kakathic
 
+search_pro(){
+if [ -e "$APK/$PTAH" ];then
+cd "$APK/$PTAH"
+ls -1d */apktool.yml */archive-info.json 2>/dev/null | sed -e 's|/apktool.yml||' -e 's|/archive-info.json||'
+fi
+}
+
+search_values(){
+path_clean="$(glog project_apk_clean)"
+if [ -e "$APK/$PTAH/$path_clean" ];then
+cd "$APK/$PTAH/$path_clean"
+ls -1d resources/package_1/res/values-*/strings.xml res/values-*/strings.xml 2>/dev/null | sed -e 's|resources/package_1/||' -e 's|res/||' -e 's|/strings.xml||'
+fi
+}
+
 # clean
 clean(){
 if [ -n "$(glog project_apk_clean)" ];then
 show_clean=1
 path_clean="$(glog project_apk_clean)"
 fi
-xml_print '
+xml_print '<?xml version="1.0" encoding="UTF-8" ?>
+<group>
 <group>
 <action shell="hidden" reload="true">
 <title>'$clean_text_1'</title>
 <summary>'$clean_text_2' '"$path_clean"'</summary>
-<param name="project" label="'$select_text_1'" options-sh="findfile file $APK/$PTAH '"'/apktool.yml|/archive-info.json'"' | sed -e '"'s|/apktool.yml||'"' -e '"'s|/archive-info.json||'"' " value-sh="glog project_apk_clean" required="true" />
+<param name="project" label="'$select_text_1'" options-sh="'$MPAT'/index.sh search_pro" value-sh="glog project_apk_clean" required="true" />
 <set>slog project_apk_clean "$project"</set>
 </action>
 </group>
@@ -20,7 +36,7 @@ xml_print '
 <group>
 <action reload="true" visible="echo '$show_clean'">
 <title>'$clean_text_3'</title>
-<param name="LIST" desc="'$clean_text_4'" multiple="multiple" options-sh="findfile file $APK/$PTAH/'$path_clean' '"'resources/package_1/res/values-.*/strings.xml|res/values-.*/strings.xml'"' | sed -e '"'s|resources/package_1/||'"' -e '"'s|res/||'"' -e '"'s|/strings.xml||'"' "/>
+<param name="LIST" desc="'$clean_text_4'" multiple="multiple" options-sh="'$MPAT'/index.sh search_values"/>
 <set>
 for vv in $LIST; do
 echo "'$clean_text_5' $vv"
@@ -32,13 +48,17 @@ fi
 done
 </set>
 </action>
+</group>
 </group>'
 }
 
 # home
 home(){
-xml_print '<group title="'$google_text'">
+xml_print '<?xml version="1.0" encoding="UTF-8" ?>
+<group>
+<group title="'$google_text'">
 <page title="'$home_text_1'" config-sh="'$MPAT'/index.sh clean"/>
+</group>
 </group>'
 }
 
@@ -54,14 +74,8 @@ trans_add "$MPAT"
 fi
 
 # index
-echo '<?xml version="1.0" encoding="UTF-8" ?>
-<group>'
-case "$1" in
-    home|clean)
-        "$1"
-        ;;
-    *)
-        cat "$ETC/error.xml"
-        ;;
-esac
-echo '</group>'
+if [ "$(type -t "$1")" = "function" ];then
+"$@"
+else
+cat "$ETC/error.xml"
+fi
