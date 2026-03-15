208215f6184ed15046b119cfbdc5aab66de39c31
Zenlua <kakathic@gmail.com>
2026-03-15 11:16:53 +0700

    Upload file

--- common/src/main/res/1.sh
+++
@@ -1,2 +0,0 @@
-
-sed -i "s|1769472|30|g" /data/user/0/com.termux/files/home/Tool-Tree/pio/src/main/res/anim/*.xml
--- krscript/src/main/java/com/omarea/krscript/ui/ListItemView.kt
+++ krscript/src/main/java/com/omarea/krscript/ui/ListItemView.kt
@@ -7,6 +7,10 @@ import android.widget.TextView
 import com.omarea.krscript.R
 import com.omarea.krscript.executor.ScriptEnvironmen
 import com.omarea.krscript.model.NodeInfoBase
+import android.graphics.RenderEffect
+import android.graphics.Shader
+import android.os.Build
+import com.tool.tree.ThemeConfig
 
 open class ListItemView(private val context: Context,
                         layoutId: Int,
@@ -87,5 +91,19 @@ open class ListItemView(private val context: Context,
         title = config.title
         desc = config.desc
         summary = config.summary
+    
+        val content = layout.findViewById<View>(android.R.id.content)
+    
+        val useWallpaper = ThemeConfig(context).getAllowTransparentUI()
+    
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S && useWallpaper) {
+            content?.setRenderEffect(
+                RenderEffect.createBlurEffect(
+                    25f,
+                    25f,
+                    Shader.TileMode.CLAMP
+                )
+            )
+        }
     }
 }
