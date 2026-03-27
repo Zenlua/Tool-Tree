bfcc7bb305dcfcf1de6d9062ef9feb5c5cc0738e
Zenlua <kakathic@gmail.com>
2026-03-27 14:50:00 +0700

    Update

--- .github/modun.sh
+++ .github/modun.sh
@@ -14,9 +14,8 @@ rm -fr "$vnk"
 done
 
 mkdir -p lib log root tmp TREE/ROM TOOL/APK usr
-chmod 755 .*
 
-7z a -t7z -y ../module.7z
+#7z a -t7z -y ../module.7z
 rm -fr ../module
 mv ../module.7z ../module
 ls -lh ../module
