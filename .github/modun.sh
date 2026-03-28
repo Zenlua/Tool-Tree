bf2a95ec5b6ebe2413b3315fbf69253f97006afc
Zenlua <kakathic@gmail.com>
2026-03-28 14:57:40 +0700

    Update

--- .github/modun.sh
+++ .github/modun.sh
@@ -3,13 +3,13 @@ cd app/src/main/assets/module
 
 for vmk in $(find etc/*.jar .local/share/apktool/framework/*.apk -type f); do
 mkdir -p "${vmk%.*}®${vmk##*.}_7zv2"
-7z x "$vmk" -o"${vmk%.*}®${vmk##*.}_7zv2" -y
+7z x "$vmk" -o"${vmk%.*}®${vmk##*.}_7zv2" -y >/dev/null
 rm -fr "$vmk"
 done
 
 for vnk in $(find etc/apkeditor®jar_7zv2/frameworks/android/*apk etc/apktool®jar_7zv2/prebuilt/*.jar -type f); do
 mkdir -p "${vnk%.*}®${vnk##*.}_7zv1"
-7z x "$vnk" -o"${vnk%.*}®${vnk##*.}_7zv1" -y
+7z x "$vnk" -o"${vnk%.*}®${vnk##*.}_7zv1" -y >/dev/null
 rm -fr "$vnk"
 done
 
