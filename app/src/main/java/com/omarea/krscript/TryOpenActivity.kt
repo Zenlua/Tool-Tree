49194de63b61fe88995e024eda2ee0d7dc3e0f69
Zenlua <kakathic@gmail.com>
2026-03-28 12:16:50 +0700

    Update

--- app/src/main/java/com/omarea/krscript/TryOpenActivity.kt
+++ app/src/main/java/com/omarea/krscript/TryOpenActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import android.widget.Toast
 import com.omarea.common.shell.KeepShellPublic
+import com.tool.tree.R
 
 class TryOpenActivity(private val context:  Context, private val activity:String) {
     private fun getIntent(): Intent? {
