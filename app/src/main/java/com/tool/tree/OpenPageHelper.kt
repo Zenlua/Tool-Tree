2322c66217647297fc4e76b6a15fa94ad25c300b
Zenlua <kakathic@gmail.com>
2026-03-29 12:12:36 +0700

    Update

--- app/src/main/java/com/tool/tree/OpenPageHelper.kt
+++ app/src/main/java/com/tool/tree/OpenPageHelper.kt
@@ -6,23 +6,20 @@ import android.os.Handler
 import android.os.Looper
 import android.widget.Toast
 import com.omarea.krscript.model.PageNode
-import com.omarea.common.ui.ProgressBarDialog
 
 class OpenPageHelper(private val activity: Activity) {
-    private val progressBarDialog by lazy { ProgressBarDialog(activity) }
 
     fun openPage(pageNode: PageNode) {
         try {
-            val intent: Intent? = when {
+            val intent = when {
                 pageNode.onlineHtmlPage.isNotEmpty() -> {
-                    progressBarDialog.showDialog(activity.getString(R.string.please_wait))
-                    // Tạo Intent
                     Intent(activity, ActionPageOnline::class.java).apply {
                         putExtra("config", pageNode.onlineHtmlPage)
                     }
                 }
 
-                pageNode.pageConfigSh.isNotEmpty() || pageNode.pageConfigPath.isNotEmpty() -> {
+                pageNode.pageConfigSh.isNotEmpty() ||
+                pageNode.pageConfigPath.isNotEmpty() -> {
                     Intent(activity, ActionPage::class.java)
                 }
 
@@ -33,13 +30,6 @@ class OpenPageHelper(private val activity: Activity) {
                 addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
                 putExtra("page", pageNode)
                 activity.startActivity(this)
-            }
-
-            // Ẩn dialog sau khi startActivity
-            if (pageNode.onlineHtmlPage.isNotEmpty()) {
-                Handler(Looper.getMainLooper()).post {
-                    progressBarDialog.hideDialog()
-                }
             }
 
         } catch (ex: Exception) {
