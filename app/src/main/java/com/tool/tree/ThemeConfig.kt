d74fc1480dfac8fd9e04080fb6906929dc260942
Zenlua <kakathic@gmail.com>
2026-04-03 02:32:34 +0700

    Update

--- app/src/main/java/com/tool/tree/ThemeConfig.kt
+++ app/src/main/java/com/tool/tree/ThemeConfig.kt
@@ -12,7 +12,7 @@ class ThemeConfig(private val context: Context) {
 
     fun setAllowTransparentUI(allow: Boolean) {
         config.edit { putBoolean("TransparentUI", allow) }
-        (context as? MainActivity)?.reloadTabs()
+        (context as? AppCompatActivity)?.recreate()
     }
 
     fun getAllowNotificationUI(): Boolean {
@@ -21,7 +21,6 @@ class ThemeConfig(private val context: Context) {
 
     fun setAllowNotificationUI(allow: Boolean) {
         config.edit { putBoolean("NotificationUI", allow) }
-        (context as? MainActivity)?.reloadTabs()
         val appContext = context.applicationContext
         if (allow) {
             WakeLockService.startService(appContext)
