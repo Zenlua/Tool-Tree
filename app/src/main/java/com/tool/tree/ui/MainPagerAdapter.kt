d8bfac7ade11ecdf4c42c09b844df2f59424e0eb
Zenlua <kakathic@gmail.com>
2026-04-03 00:15:16 +0700

    Update

--- app/src/main/java/com/tool/tree/ui/MainPagerAdapter.kt
+++ app/src/main/java/com/tool/tree/ui/MainPagerAdapter.kt
@@ -9,45 +9,31 @@ class MainPagerAdapter(activity: AppCompatActivity) : FragmentStateAdapter(activ
     private val fragments = mutableListOf<Fragment>()
     private val titles = mutableListOf<String>()
 
-    /**
-     * Thêm 1 tab
-     */
     fun addFragment(fragment: Fragment, title: String) {
         fragments.add(fragment)
         titles.add(title)
     }
 
-    /**
-     * Tổng số tab
-     */
     override fun getItemCount(): Int {
         return fragments.size
     }
 
-    /**
-     * Trả về Fragment theo vị trí
-     */
     override fun createFragment(position: Int): Fragment {
         return fragments[position]
     }
 
-    /**
-     * Lấy title cho TabLayout
-     */
     fun getTitle(position: Int): String {
         return titles[position]
     }
 
-    /**
-     * (Optional) Lấy fragment nếu cần xử lý ngoài
-     */
     fun getFragment(position: Int): Fragment {
         return fragments[position]
     }
 
-    /**
-     * (Optional) Xoá toàn bộ tab (dùng khi reload)
-     */
+    fun getTitleIndex(title: String): Int {
+        return titles.indexOf(title)
+    }
+
     fun clear() {
         fragments.clear()
         titles.clear()
