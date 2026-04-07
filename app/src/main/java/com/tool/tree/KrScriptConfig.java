53068437f237f1982a8e369bd1c16d4e5ca6bd04
Zenlua <kakathic@gmail.com>
2026-04-07 22:01:18 +0700

    Update

--- app/src/main/java/com/tool/tree/KrScriptConfig.java
+++ app/src/main/java/com/tool/tree/KrScriptConfig.java
@@ -20,12 +20,20 @@ public class KrScriptConfig {
     private final static String PAGE_LIST_CONFIG_SH = "page_list_config_sh";
     private final static String FAVORITE_CONFIG = "favorite_config";
     private final static String FAVORITE_CONFIG_SH = "favorite_config_sh";
+    private final static String CUSTOM_TAB3_CONFIG = "custom_tab3_config";
+    private final static String CUSTOM_TAB3_CONFIG_SH = "custom_tab3_config_sh";
+    private final static String CUSTOM_TAB4_CONFIG = "custom_tab4_config";
+    private final static String CUSTOM_TAB4_CONFIG_SH = "custom_tab4_config_sh";
     private final static String BEFORE_START_SH = "before_start_sh";
+
     private static HashMap<String, String> configInfo;
+
     private final String EXECUTOR_CORE_DEFAULT = "file:///android_asset/root/executor.sh";
     private final String PAGE_LIST_CONFIG_DEFAULT = "file:///android_asset/root/more.xml";
     private final String FAVORITE_CONFIG_DEFAULT = "file:///android_asset/root/favorites.xml";
-    private final String BEFORE_START_SH_DEFAULT = ""; //"file:///android_asset/kr-script/before_start.sh";
+    private final String CUSTOM_TAB3_DEFAULT = "file:///android_asset/root/tab3.xml";
+    private final String CUSTOM_TAB4_DEFAULT = "file:///android_asset/root/tab4.xml";
+    private final String BEFORE_START_SH_DEFAULT = "";
 
     public KrScriptConfig init(Context context) {
         if (configInfo == null) {
@@ -33,6 +41,8 @@ public class KrScriptConfig {
             configInfo.put(EXECUTOR_CORE, EXECUTOR_CORE_DEFAULT);
             configInfo.put(PAGE_LIST_CONFIG, PAGE_LIST_CONFIG_DEFAULT);
             configInfo.put(FAVORITE_CONFIG, FAVORITE_CONFIG_DEFAULT);
+            configInfo.put(CUSTOM_TAB3_CONFIG, CUSTOM_TAB3_DEFAULT);
+            configInfo.put(CUSTOM_TAB4_CONFIG, CUSTOM_TAB4_DEFAULT);
             configInfo.put(TOOLKIT_DIR, TOOLKIT_DIR_DEFAULT);
             configInfo.put(BEFORE_START_SH, BEFORE_START_SH_DEFAULT);
 
@@ -56,7 +66,9 @@ public class KrScriptConfig {
                     }
                 }
             } catch (Exception ex) {
+                ex.printStackTrace();
             }
+
             ScriptEnvironmen.init(context, getExecutorCore(), getToolkitDir());
         }
 
@@ -103,6 +115,34 @@ public class KrScriptConfig {
             }
             if (configInfo.containsKey(FAVORITE_CONFIG)) {
                 pageInfo.setPageConfigPath(configInfo.get(FAVORITE_CONFIG));
+            }
+            return pageInfo;
+        }
+        return null;
+    }
+
+    public PageNode getCustomTab3Config() {
+        if (configInfo != null) {
+            PageNode pageInfo = new PageNode("");
+            if (configInfo.containsKey("custom_tab3_config_sh")) {
+                pageInfo.setPageConfigSh(configInfo.get("custom_tab3_config_sh"));
+            }
+            if (configInfo.containsKey(CUSTOM_TAB3_CONFIG)) {
+                pageInfo.setPageConfigPath(configInfo.get(CUSTOM_TAB3_CONFIG));
+            }
+            return pageInfo;
+        }
+        return null;
+    }
+    
+    public PageNode getCustomTab4Config() {
+        if (configInfo != null) {
+            PageNode pageInfo = new PageNode("");
+            if (configInfo.containsKey("custom_tab4_config_sh")) {
+                pageInfo.setPageConfigSh(configInfo.get("custom_tab4_config_sh"));
+            }
+            if (configInfo.containsKey(CUSTOM_TAB4_CONFIG)) {
+                pageInfo.setPageConfigPath(configInfo.get(CUSTOM_TAB4_CONFIG));
             }
             return pageInfo;
         }
