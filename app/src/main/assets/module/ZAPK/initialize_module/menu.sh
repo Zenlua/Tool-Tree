728560f9544d7b48cac0e73eb3c1a85fcfaffa8e
Zenlua <kakathic@gmail.com>
2026-07-04 18:45:37 +0700

    Update

--- app/src/main/assets/module/ZAPK/initialize_module/menu.sh
+++ app/src/main/assets/module/ZAPK/initialize_module/menu.sh
@@ -1,11 +1,15 @@
 # Kakathic
 
 MPAT="${0%/*}"
+path_modun="/data/adb/modules/Tool-Tree"
+path_modun2="/data/adb/modules_update/Tool-Tree"
+
 if [ "$1" == "code_option" ];then
 echo '<option type="default" id="vip123" reload="true" >@string/remove_text</option>'
 elif [ "$1" == "code_shell" ];then
 echo 'if [ "$menu_id" == "vip123" ]; then
-touch /data/adb/modules/Tool-Tree/remove
+[ -d '$path_modun' ] && touch '$path_modun'/remove
+[ -d '$path_modun2' ] && rm -fr '$path_modun2'
 echo "The module will be removed after the device restarts."
 fi'
 fi
