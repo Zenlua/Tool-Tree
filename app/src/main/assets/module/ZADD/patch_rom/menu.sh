267d9f26f5c7dddabc4bdf2f4bc49e6f6e7712a9
Zenlua <kakathic@gmail.com>
2026-07-06 23:19:13 +0700

    Update

--- app/src/main/assets/module/ZADD/patch_rom/menu.sh
+++ app/src/main/assets/module/ZADD/patch_rom/menu.sh
@@ -2,11 +2,7 @@
 
 MPAT="${0%/*}"
 if [ "$1" == "code_option" ];then
-echo '
-<option type="default" id="vip2" auto-off="true" reload="true" interruptible="false">@string/update_text</option>
-'
+echo '<option type="default" id="vip2" auto-off="true" reload="true" interruptible="true">@string/update_text</option>'
 elif [ "$1" == "code_shell" ];then
-echo '
-[ "$menu_id" == "vip2" ] && '$MPAT'/index.sh update
-'
+echo '[ "$menu_id" == "vip2" ] && '$MPAT'/index.sh update'
 fi
