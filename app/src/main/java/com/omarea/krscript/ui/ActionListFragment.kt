4bed720ba78b159bd80670eb278dd9dcef787bac
Zenlua <kakathic@gmail.com>
2026-07-22 15:41:49 +0700

    Update

--- app/src/main/java/com/omarea/krscript/ui/ActionListFragment.kt
+++ app/src/main/java/com/omarea/krscript/ui/ActionListFragment.kt
@@ -215,7 +215,7 @@ class ActionListFragment : androidx.fragment.app.Fragment(), PageLayoutRender.On
                 Toast.makeText(context, getString(R.string.editor_file_missing), Toast.LENGTH_SHORT).show()
                 return
             }
-            com.tool.tree.TextEditorActivity.start(context, item.file, item.title, item.desc, item.wrap, item.pageConfigDir)
+            com.tool.tree.TextEditorActivity.start(context, item.file, item.title, item.desc, item.wrap, item.pageConfigDir, item.placeholder)
             onCompleted.run()
         }
     }
