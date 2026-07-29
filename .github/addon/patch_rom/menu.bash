d1ac8578e7ac84449c239254cbfc690181653d18
Zenlua <kakathic@gmail.com>
2026-07-29 23:45:49 +0700

    Update

--- .github/addon/patch_rom/menu.bash
+++ .github/addon/patch_rom/menu.bash
@@ -3,13 +3,9 @@
 MPAT="${0%/*}"
   echo '
   [[group.page.options]]
-  type = "refresh"
-  style = "fab"
-  icon = "'$ETC'/icon/Loading.png"
-  
-  [[group.page.options]]
   type = "default"
   title = "@string/update_text add-on"
   auto-finish = true
-  script = "'$MPAT'/index.bash update_addon" '
+  script = "'$MPAT'/index.bash update_addon"
+  '
   
