9181d96127142d55af5fd177559433b1d99c8757
Zenlua <kakathic@gmail.com>
2026-08-01 20:36:43 +0700

    Update

--- .github/plugin/ZADD/add_features/index.bash
+++ .github/plugin/ZADD/add_features/index.bash
@@ -11,7 +11,7 @@ title = "'$google_text'"
   script = """
     slog silencekd "$silence"
     slog dang_filehd "$dang_file"
-    '$MPAT'/bin/combine_img $silence $dang_file "$MUTIIMG" "$IMAGE"
+    MPAT="'$MPAT'" '$MPAT'/bin/combine_img $silence $dang_file "$MUTIIMG" "$IMAGE"
     checktime
   """
   [[group.action.params]]
