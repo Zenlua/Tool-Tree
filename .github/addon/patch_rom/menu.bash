887ea60aecf03102d113703ef82227dca22d25a8
Zenlua <kakathic@gmail.com>
2026-08-02 22:46:26 +0700

    Update

--- .github/addon/patch_rom/menu.bash
+++ .github/addon/patch_rom/menu.bash
@@ -6,13 +6,13 @@ MPAT="${0%/*}"
   type = "default"
   title = "@string/update_text"
   script = """
+  check_add "'$MPAT'"
+  echo
   echo "English:"
   cat '$MPAT'/changelog.txt
   echo
   echo "$LANGUAGE-$COUNTRY"
   cat '$MPAT'/changelog.txt | trans $LANGUAGE-$COUNTRY
-  echo
-  check_add "'$MPAT'"
   """
   '
   
