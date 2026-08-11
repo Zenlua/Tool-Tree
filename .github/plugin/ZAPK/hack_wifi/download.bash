872d00f561e3b7244f695e2b6aab3910bbb36968
Zenlua <kakathic@gmail.com>
2026-08-12 00:09:41 +0700

    Update

--- .github/plugin/ZAPK/hack_wifi/download.bash
+++ .github/plugin/ZAPK/hack_wifi/download.bash
@@ -7,4 +7,4 @@ description="Wi-Fi hacking tool"
 version=1.0
 versionCode=100
 root=true
-url=https://github.com/Zenlua/Tool-Tree/releases/download/V1/hack_wifi.add
+url=https://github.com/Zenlua/Tool-Tree/releases/download/V1/hack_wifi_$PACKAGE_VERSION_NAME.add
