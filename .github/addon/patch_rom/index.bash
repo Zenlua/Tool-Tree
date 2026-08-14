8ae991120d0693940bca813f009ecd9f62b4bf74
Zenlua <kakathic@gmail.com>
2026-08-14 21:51:18 +0700

    Update

--- .github/addon/patch_rom/index.bash
+++ .github/addon/patch_rom/index.bash
@@ -466,7 +466,7 @@ home() {
     name = "kill_apk_list"
     title = "'"$param1_title"'"
     label = "'"$param1_label"'"
-    desc = "'$string_text_1': Project/'$PTSH', '$sdcard_text'"
+    desc = "'$string_text_1': Project/'$PTSH', '$sdcard_text'/out"
     options-sh = "'$pathsh' list_apk_file"
     value-sh = "glog kill_apk_list"
     required = true
