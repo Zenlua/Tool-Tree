b7e99541a0f036ad713bbe6d8b0ece56ea0e202e
Zenlua <kakathic@gmail.com>
2026-08-17 17:18:16 +0700

    Update

---
+++ .github/plugin/ZAPK/theme_trans/download.bash
@@ -0,0 +1,10 @@
+# Add-on
+
+id=theme_trans
+name="Theme Trans"
+author=Kakathic
+description="Translate theme, widget, aod"
+version=1.0
+versionCode=100
+root=true
+url=https://github.com/Zenlua/Tool-Tree/releases/download/V1/${id}_$versionCode.add
